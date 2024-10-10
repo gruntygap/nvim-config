b6724daffc2615f7fda9a233df0ccc772bcbeb85
Grant Gapinski <grantg@steerbridge.com>
2024-10-10 14:52:23 -0500

    Create git blame shortcut, and change tab width to two

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,2 +1,5 @@
+-- Git status
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git);
 
+-- Git blame
+vim.keymap.set('n', '<leader>gb', '<cmd>Git blame<cr>');
--- lua/granted/set.lua
+++ lua/granted/set.lua
@@ -6,11 +6,13 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 
 vim.opt.expandtab = true
-vim.opt.tabstop = 4
+vim.opt.tabstop = 2
 vim.opt.softtabstop = 0
-vim.opt.shiftwidth = 4
--- vim.opt.smarttab = true
--- vim.opt.smartindent = true
+vim.opt.shiftwidth = 2
+
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+
 vim.opt.wrap = false
 
 vim.opt.swapfile = false
