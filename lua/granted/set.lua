4b62a471af7156348325dcbeee17bb3c83f2d136
gruntygap <gruntygap@gmail.com>
2024-10-22 03:21:48 -0500

    Turned on hlsearch

--- lua/granted/set.lua
+++ lua/granted/set.lua
@@ -23,7 +23,8 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
+-- turn on highlighting all matches
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
