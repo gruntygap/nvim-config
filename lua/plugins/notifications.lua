f5f75ec15d90caf4a7876f0de46cc2b8c73bef06
gruntygap <gruntygap@gmail.com>
2024-12-13 16:43:52 -0600

    Chg from fidget to mini.notify

--- lua/plugins/notifications.lua
+++ lua/plugins/notifications.lua
@@ -1,8 +1,13 @@
 return {
-  'j-hui/fidget.nvim',
-  opts = {
-    notification = {
-      override_vim_notify = true
-    }
+  -- 'j-hui/fidget.nvim',
+  -- lazy = true,
+  -- opts = {
+  --   notification = {
+  --     override_vim_notify = true
+  --   }
+  -- },
+  {
+    'echasnovski/mini.notify',
+    opts = {}
   }
 }
