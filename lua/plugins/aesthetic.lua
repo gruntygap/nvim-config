934827da288e42ed8f0cb584faaa19f9e5dff832
gruntygap <gruntygap@gmail.com>
2024-12-13 21:34:06 -0600

    Added indent-blankline

--- lua/plugins/aesthetic.lua
+++ lua/plugins/aesthetic.lua
@@ -29,5 +29,14 @@ return {
   },
   {
     "folke/zen-mode.nvim"
+  },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    event = { 'BufReadPre', 'BufNewFile' },
+    lazy = true,
+    ---@module 'ibl'
+    ---@type ibl.config
+    opts = {}
   }
 }
