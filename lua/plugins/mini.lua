91c586a9c01966a924b7fe925eb6af27a9c79c54
gruntygap <gruntygap@gmail.com>
2024-12-15 03:35:47 -0600

    make mini lazy

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,10 +1,14 @@
 return {
   {
     'echasnovski/mini.surround',
+    lazy = true,
+    event = { 'BufReadPre', 'BufNewFile' },
     opts = {}
   },
   {
     'echasnovski/mini.pairs',
+    lazy = true,
+    event = { 'BufReadPre', 'BufNewFile' },
     opts = {}
   }
 }
