21c81d69c372aabd4a53de07cf0a01e22637170f
gruntygap <gruntygap@gmail.com>
2025-05-08 09:55:18 -0500

    Swap to blink.cmp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -114,14 +114,14 @@ return {
         }
       }
 
-      local capabilities = vim.tbl_deep_extend(
-        'force',
-        require('lspconfig').util.default_config.capabilities,
-        require('cmp_nvim_lsp').default_capabilities()
-      )
+      -- local capabilities = vim.tbl_deep_extend(
+      --   'force',
+      --   require('lspconfig').util.default_config.capabilities,
+      --   require('cmp_nvim_lsp').default_capabilities()
+      -- )
 
       for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
-        local config = { capabilities = capabilities }
+        local config = {}
 
         if custom_configs[server] then
           for k, v in pairs(custom_configs[server]) do
@@ -163,73 +163,126 @@ return {
   },
   -- Autocompletion
   {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      -- 'hrsh7th/cmp-path',
-      --'hrsh7th/cmp-cmdline',
-      -- Not sure that I need these ^
-      -- Snippets
-      'L3MON4D3/LuaSnip',
-      'rafamadriz/friendly-snippets',
-      'saadparwaiz1/cmp_luasnip',
-      -- Format help (adds type and icon to dropdown)
-      'onsails/lspkind.nvim'
+    'saghen/blink.cmp',
+    -- optional: provides snippets for the snippet source
+    dependencies = { 'rafamadriz/friendly-snippets' },
+
+    -- use a release tag to download pre-built binaries
+    version = '1.*',
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = { preset = 'default' },
+
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
+      },
+
+      -- (Default) Only show the documentation popup when manually triggered
+      completion = { documentation = { auto_show = true } },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { 'lsp', 'buffer', 'path', 'snippets' },
+      },
+
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = "prefer_rust_with_warning" }
     },
-    lazy = true,
-    event = 'InsertEnter',
-    config = function()
-      -- Setup completion
-      local cmp = require('cmp')
-      local lspkind = require('lspkind')
-      local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-      cmp.setup({
-        formatting = {
-          format = lspkind.cmp_format({
-            mode = 'symbol_text', -- show only symbol annotations
-            maxwidth = {
-              -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-              -- can also be a function to dynamically calculate max width such as
-              -- menu = function() return math.floor(0.45 * vim.o.columns) end,
-              menu = 50,              -- leading text (labelDetails)
-              abbr = 50,              -- actual suggestion item
-            },
-            ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-            show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-
-            -- The function below will be called before any actual modifications from lspkind
-            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-            before = function(entry, vim_item)
-              vim_item.menu = entry.source.name
-              return vim_item
-            end
-          })
-        },
-        sources = {
-          { name = 'nvim_lsp' },
-          { name = 'buffer' }, -- cmp-buffer
-          { name = 'luasnip' } -- cmp_luasnip
-        },
-        snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-          ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-          ['<C-Space>'] = cmp.mapping.complete(),
-        }),
-      })
-
-      require('luasnip.loaders.from_vscode').lazy_load()
-    end
+    opts_extend = { "sources.default" }
   },
+  -- {
+  --   'hrsh7th/nvim-cmp',
+  --   dependencies = {
+  --     'hrsh7th/cmp-nvim-lsp',
+  --     'hrsh7th/cmp-buffer',
+  --     -- 'hrsh7th/cmp-path',
+  --     --'hrsh7th/cmp-cmdline',
+  --     -- Not sure that I need these ^
+  --     -- Snippets
+  --     'L3MON4D3/LuaSnip',
+  --     'rafamadriz/friendly-snippets',
+  --     'saadparwaiz1/cmp_luasnip',
+  --     -- Format help (adds type and icon to dropdown)
+  --     'onsails/lspkind.nvim'
+  --   },
+  --   lazy = true,
+  --   event = 'InsertEnter',
+  --   config = function()
+  --     -- Setup completion
+  --     local cmp = require('cmp')
+  --     local lspkind = require('lspkind')
+  --     local cmp_select = { behavior = cmp.SelectBehavior.Select }
+  --
+  --     cmp.setup({
+  --       formatting = {
+  --         format = lspkind.cmp_format({
+  --           mode = 'symbol_text', -- show only symbol annotations
+  --           maxwidth = {
+  --             -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+  --             -- can also be a function to dynamically calculate max width such as
+  --             -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+  --             menu = 50,              -- leading text (labelDetails)
+  --             abbr = 50,              -- actual suggestion item
+  --           },
+  --           ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+  --           show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+  --
+  --           -- The function below will be called before any actual modifications from lspkind
+  --           -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+  --           before = function(entry, vim_item)
+  --             vim_item.menu = entry.source.name
+  --             return vim_item
+  --           end
+  --         })
+  --       },
+  --       sources = {
+  --         { name = 'nvim_lsp' },
+  --         { name = 'buffer' }, -- cmp-buffer
+  --         { name = 'luasnip' } -- cmp_luasnip
+  --       },
+  --       snippet = {
+  --         expand = function(args)
+  --           require('luasnip').lsp_expand(args.body)
+  --         end,
+  --       },
+  --       mapping = cmp.mapping.preset.insert({
+  --         ['<C-f>'] = cmp.mapping.scroll_docs(4),
+  --         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+  --         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  --         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  --         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  --         ['<C-Space>'] = cmp.mapping.complete(),
+  --       }),
+  --     })
+  --
+  --     require('luasnip.loaders.from_vscode').lazy_load()
+  --   end
+  -- },
   -- LSP Server Management Application
   {
     'mason-org/mason.nvim',
